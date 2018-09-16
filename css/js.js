04e99fc41e66d8a109f40d7e6939737b16c500bb
Sergeyingit <sergeyakkaynt@gmail.com>
2018-09-16 19:11:30 +0300

    Добавляет карты и жс

---
+++ css/js.js
@@ -0,0 +1,70 @@
+var link = document.querySelector(".contacts .btn");
+var popup = document.querySelector(".back-communication");
+var closePopup = popup.querySelector(".close-btn");
+var name = popup.querySelector("[name=name]");
+var form = popup.querySelector("form");
+var email = popup.querySelector("[name=email]");
+var letter = popup.querySelector("[name=letter]");
+var shadow = document.querySelector(".shadow");
+
+var isStorageSupport = true;
+var storage = "";
+var storageEmail = "";
+
+try {
+	storage = localStorage.getItem("name");
+	storageEmail = localStorage.getItem("email");
+} catch (err) {
+	isStorageSupport = false;
+}
+
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    shadow.classList.add("shadow-on");
+    popup.classList.add("modal-show");
+    if (storage) {
+    	name.value = storage;
+    	if (storageEmail) {
+    		email.value = storageEmail;
+    		letter.focus();
+    	} else {
+    		email.focus();
+    	}
+
+    } else {
+    name.focus();
+    }
+});
+
+closePopup.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	shadow.classList.remove("shadow-on");
+	popup.classList.remove("modal-show");
+	popup.classList.remove ("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!name.value || !email.value || !letter.value) {
+	evt.preventDefault();
+	popup.classList.remove("modal-error");
+	popup.offsetWidth = popup.offsetWidth;
+	popup.classList.add("modal-error");
+    } else {
+    	if (isStorageSupport) {
+    		localStorage.setItem("name", name.value);
+    		localStorage.setItem("email", email.value);
+    	}
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (popup.classList.contains("modal-show")) {
+			shadow.classList.remove("shadow-on");
+			popup.classList.remove("modal-show");
+			popup.classList.remove("modal-error");
+		}
+	}
+});
